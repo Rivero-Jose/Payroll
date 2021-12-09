dc306daca97ad112a7aa77a0867fbf0022e8ae18
Jose Rivero <jrive387@fiu.edu>
2021-12-09 17:31:26 -0500

    init commit

---
+++ src/FinalMain.java
@@ -0,0 +1,7 @@
+
+public class FinalMain {
+	public static void main(String args[]) {
+		Roll R = new Roll();
+		R.start();	
+	}
+}
---
+++ src/Roll.java
@@ -0,0 +1,11 @@
+import javax.swing.ImageIcon;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+public class Roll {
+	ArrayList<Roll> workers = new ArrayList<Roll>();
+	public void start(){
+		
+	}
+}
